e0c126d2a46f739fecbb2e75406bea02f5d99a12
perissology <perissology@protonmail.com>
2017-12-14 06:14:26 -0800

    code cleanup

--- contracts/LPPCappedMilestones.sol
+++ contracts/LPPCappedMilestones.sol
@@ -1,12 +1,33 @@
 pragma solidity ^0.4.17;
 
+/*
+    Copyright 2017, RJ Ewing <perissology@protonmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import "liquidpledging/contracts/LiquidPledging.sol";
 import "giveth-common-contracts/contracts/Escapable.sol";
 
+
 contract LPPCappedMilestones is Escapable {
     uint constant FROM_OWNER = 0;
+
     uint constant FROM_INTENDEDPROJECT = 255;
+
     uint constant TO_OWNER = 256;
+
     uint constant TO_INTENDEDPROJECT = 511;
 
     LiquidPledging public liquidPledging;
@@ -21,12 +42,7 @@ contract LPPCappedMilestones is Escapable {
         bool accepted;
     }
 
-    mapping(uint64 => Milestone) milestones;
-
-    //TODO add code to change addresses for a milestone
-//    address public newreviewer;
-//    address public newCampaignReviewer;
-//    address public newRecipient;
+    mapping (uint64 => Milestone) milestones;
 
 
     event MilestoneAccepted(uint64 indexed idProject);
@@ -41,18 +57,11 @@ contract LPPCappedMilestones is Escapable {
         liquidPledging = _liquidPledging;
     }
 
-    function addMilestone(
-        string name,
-        string url,
-        uint _maxAmount,
-        uint64 parentProject,
-        address _recipient,
-        address _reviewer,
-        address _campaignReviewer
-    ) public {
-        uint64 idProject = liquidPledging.addProject(name, url, address(this), parentProject, uint64(0), ILiquidPledgingPlugin(this));
-        milestones[idProject] = Milestone(_maxAmount, 0, 0, _reviewer, _campaignReviewer, _recipient, false);
-    }
+    //== fallback
+
+    function() payable {}
+
+    //== external
 
     /// @dev Plugins are used (much like web hooks) to initiate an action
     ///  upon any donation, delegation, or transfer; this is an optional feature
@@ -75,35 +84,31 @@ contract LPPCappedMilestones is Escapable {
         uint64 pledgeTo,
         uint64 context,
         uint amount
-    ) external returns (uint maxAllowed){
+    ) external returns (uint maxAllowed)
+    {
         require(msg.sender == address(liquidPledging));
-        var (, , , fromIntendedProject , , , ) = liquidPledging.getPledge(pledgeFrom);
-        var (, toOwner , , toIntendedProject , , , toPledgeState ) = liquidPledging.getPledge(pledgeTo);
+        var (,,, fromIntendedProject,,,) = liquidPledging.getPledge(pledgeFrom);
+        var (, toOwner,, toIntendedProject,,, toPledgeState) = liquidPledging.getPledge(pledgeTo);
         Milestone storage m;
 
         // If it is proposed or comes from somewhere else of a proposed project, do not allow.
         // only allow from the proposed project to the project in order normalize it.
         // don't need to check if canceled b/c lp does this
         if (context == TO_INTENDEDPROJECT) {
-            m = milestones[ toIntendedProject ];
+            m = milestones[toIntendedProject];
             if (m.accepted) {
                 return 0;
             }
-        } else if (context == TO_OWNER) {
-            if (fromIntendedProject != toOwner &&
-                    toPledgeState == LiquidPledgingBase.PledgeState.Pledged) {
-                //TODO what if milestone isn't initialized? should we throw?
-                // this can happen if someone adds a project through lp with this contracts address as the plugin
-                // we can require(maxAmount > 0);
-                // don't need to check if canceled b/c lp does this
-                m = milestones[ toOwner ];
-                if (m.accepted) {
-                    return 0;
-                }
-            } else if (toPledgeState == LiquidPledgingBase.PledgeState.Paying) {
-//                 only accepted milestones can be moved to paying status
-                m = milestones[ toOwner ];
-                require(m.accepted);
+        } else if (context == TO_OWNER &&
+        (fromIntendedProject != toOwner &&
+        toPledgeState == LiquidPledgingBase.PledgeState.Pledged)) {
+            //TODO what if milestone isn't initialized? should we throw?
+            // this can happen if someone adds a project through lp with this contracts address as the plugin
+            // we can require(maxAmount > 0);
+            // don't need to check if canceled b/c lp does this
+            m = milestones[toOwner];
+            if (m.accepted) {
+                return 0;
             }
         }
         return amount;
@@ -120,19 +125,20 @@ contract LPPCappedMilestones is Escapable {
         uint64 pledgeTo,
         uint64 context,
         uint amount
-    ) external {
+    ) external
+    {
         uint returnFunds;
         require(msg.sender == address(liquidPledging));
 
-        var (, oldOwner, , , , , ) = liquidPledging.getPledge(pledgeFrom);
-        var (, toOwner , , , , , toPledgeState) = liquidPledging.getPledge(pledgeTo);
+        var (, oldOwner,,,,,) = liquidPledging.getPledge(pledgeFrom);
+        var (, toOwner,,,,, toPledgeState) = liquidPledging.getPledge(pledgeTo);
 
-        if (context == TO_OWNER ) {
+        if (context == TO_OWNER) {
             Milestone storage m;
 
             // Recipient of the funds from a different owner
             if (oldOwner != toOwner) {
-                m = milestones[ toOwner ];
+                m = milestones[toOwner];
 
                 m.received += amount;
                 if (m.accepted) {
@@ -143,22 +149,54 @@ contract LPPCappedMilestones is Escapable {
                     returnFunds = 0;
                 }
 
-                if (returnFunds > 0) {  // Sends exceeding money back
+                if (returnFunds > 0) {// Sends exceeding money back
                     m.received -= returnFunds;
                     liquidPledging.cancelPledge(pledgeTo, returnFunds);
                 }
             } else if (toPledgeState == LiquidPledgingBase.PledgeState.Paid) {
-                m = milestones[ toOwner ];
+                m = milestones[toOwner];
                 m.canCollect += amount;
             }
         }
     }
 
-    function acceptMilestone(uint64 idProject) {
+    //== public
+
+    function addMilestone(
+        string name,
+        string url,
+        uint _maxAmount,
+        uint64 parentProject,
+        address _recipient,
+        address _reviewer,
+        address _campaignReviewer
+    ) public
+    {
+        uint64 idProject = liquidPledging.addProject(
+            name,
+            url,
+            address(this),
+            parentProject,
+            uint64(0),
+            ILiquidPledgingPlugin(this)
+        );
+
+        milestones[idProject] = Milestone(
+            _maxAmount,
+            0,
+            0,
+            _reviewer,
+            _campaignReviewer,
+            _recipient,
+            false
+        );
+    }
+
+    function acceptMilestone(uint64 idProject) public {
         bool isCanceled = liquidPledging.isProjectCanceled(idProject);
         require(!isCanceled);
 
-        Milestone storage m = milestones[ idProject ];
+        Milestone storage m = milestones[idProject];
         require(msg.sender == m.reviewer || msg.sender == m.campaignReviewer);
         require(!m.accepted);
 
@@ -166,8 +204,8 @@ contract LPPCappedMilestones is Escapable {
         MilestoneAccepted(idProject);
     }
 
-    function cancelMilestone(uint64 idProject) {
-        Milestone storage m = milestones[ idProject ];
+    function cancelMilestone(uint64 idProject) public {
+        Milestone storage m = milestones[idProject];
         require(msg.sender == m.reviewer || msg.sender == m.campaignReviewer);
         require(!m.accepted);
 
@@ -177,11 +215,9 @@ contract LPPCappedMilestones is Escapable {
     function withdraw(uint64 idProject, uint64 idPledge, uint amount) public {
         // we don't check if canceled here.
         // lp.withdraw will normalize the pledge & check if canceled
-        // beforeTransfer will check if milestone is accepted
-//        Milestone storage m = milestones[ idProject ];
-//        require(msg.sender == m.recipient);
-//        require(m.accepted);
-        //TODO update this and before/after transfer depending on mTransfer methods
+        Milestone storage m = milestones[idProject];
+        require(msg.sender == m.recipient);
+        require(m.accepted);
 
         liquidPledging.withdraw(idPledge, amount);
         collect(idProject);
@@ -190,52 +226,31 @@ contract LPPCappedMilestones is Escapable {
     uint constant D64 = 0x10000000000000000;
 
     function mWithdraw(uint[] pledgesAmounts) public {
-        // to save gas, we will perform any necessary checks in the beforeTransfer
-        // method. This saves us from having to iterate & make additional calls to
-        // fetch the pledge for each pledgeAmount, since we are already fetching
-        // in beforeTransfer
-        //TODO this removes the require(msg.sender == m.recipient) check
+        uint64[] memory mIds = new uint64[](pledgesAmounts.length);
+
+        for (uint i = 0; i < pledgesAmounts.length; i++ ) {
+            uint64 idPledge = uint64(pledgesAmounts[i] & (D64-1));
+            var (, idProject , , , , , ) = liquidPledging.getPledge(idPledge);
+
+            mIds[i] = idProject;
+            Milestone storage m = milestones[idProject];
+            require(msg.sender == m.recipient);
+            require(m.accepted);
+        }
+
         liquidPledging.mWithdraw(pledgesAmounts);
-        // TODO pick the above or below method
-        // withdraw -> 295154
-        // mWithdraw:
-        // 1 pledge -> 296898
-        // 2 pledge -> 568657
-        // 3 pledge -> 840482
 
-        // before/after transfer -- this however removes the require(msg.sender == m.recipient) check from withdraw & collect
-        // withdraw -> 290479
-        // mWithdraw:
-        // 1 pledge -> 290552
-        // 2 pledge -> 556230
-        // 3 pledge -> 821974
-
-        // might be better to use the before/after transfer functionality
-//        uint64[] memory mIds = new uint64[](pledgesAmounts.length);
-//
-//        for (uint i = 0; i < pledgesAmounts.length; i++ ) {
-//            uint64 idPledge = uint64( pledgesAmounts[i] & (D64-1) );
-//            var (, idProject , , , , , ) = liquidPledging.getPledge(idPledge);
-//
-//            mIds[ i ] = idProject;
-//            Milestone storage m = milestones[ idProject ];
-//            require(msg.sender == m.recipient);
-//            require(m.accepted);
-//        }
-//
-//        liquidPledging.mWithdraw(pledgesAmounts);
-//
-//        for (i = 0; i < mIds.length; i++ ) {
-//            collect(mIds[i]);
-//        }
+        for (i = 0; i < mIds.length; i++ ) {
+            collect(mIds[i]);
+        }
     }
 
     function collect(uint64 idProject) public {
-        Milestone storage m = milestones[ idProject ];
+        Milestone storage m = milestones[idProject];
         require(msg.sender == m.recipient);
 
         if (m.canCollect > 0) {
-            // TODO should this be removes?
+            // TODO should this assert be removed?
             assert(this.balance >= m.canCollect);
             uint amount = m.canCollect;
             m.canCollect = 0;
@@ -244,7 +259,7 @@ contract LPPCappedMilestones is Escapable {
         }
     }
 
-    function getMilestone(uint64 idProject) public view returns(
+    function getMilestone(uint64 idProject) public view returns (
         uint maxAmount,
         uint received,
         uint canCollect,
@@ -253,7 +268,7 @@ contract LPPCappedMilestones is Escapable {
         address recipient,
         bool accepted
     ) {
-        Milestone storage m = milestones[ idProject ];
+        Milestone storage m = milestones[idProject];
         maxAmount = m.maxAmount;
         received = m.received;
         canCollect = m.canCollect;
@@ -262,6 +277,4 @@ contract LPPCappedMilestones is Escapable {
         recipient = m.recipient;
         accepted = m.accepted;
     }
-
-    function () payable {}
 }
--- js/LPPCappedMilestones.js
+++ js/LPPCappedMilestones.js
@@ -4,31 +4,4 @@ const generateClass = require('eth-contract-class').default;
 
 const LPPCappedMilestones = generateClass(LPPCappedMilestonesAbi, LPPCappedMilestonesByteCode);
 
-// LPPCappedMilestones.prototype.getState = function () {
-//   return Promise.all([
-//       this.liquidPledging(),
-//       this.idProject(),
-//       this.maxAmount(),
-//       this.cumulatedReceived(),
-//       this.reviewer(),
-//       this.recipient(),
-//       this.newReviewer(),
-//       this.newRecipient(),
-//       this.accepted(),
-//       this.canceled(),
-//     ])
-//     .then(results => ({
-//       liquidPledging: results[0],
-//       idProject: results[1],
-//       maxAmount: results[2],
-//       cumulatedReceived: results[3],
-//       reviewer: results[4],
-//       recipient: results[5],
-//       newReviewer: results[6],
-//       newRecipient: results[7],
-//       accepted: results[8],
-//       canceled: results[9],
-//     }));
-// };
-
 module.exports = LPPCappedMilestones;
